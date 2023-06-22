15ab9adcabdb1779613e5d74423dd341c7faef01
alex <aforalex98@gmail.com>
2023-06-21 21:03:10 -0700

    8?

--- nn_preparing/gen_num.py
+++ nn_preparing/gen_num.py
@@ -4,7 +4,7 @@ import numpy as np
 
 if __name__ == "__main__":
     model = tf.keras.models.load_model("gen_num.h5")
-    input_data = np.array([[0, 0, 1, 0, 0, 0, 0, 0, 0, 0]])
+    input_data = np.array([[0, 0, 0, 0, 0, 0, 0, 1, 0, 0]])
     prediction = model.predict(input_data)
     num = prediction.reshape((28, 28))
-    plt.imsave("image.png", num, cmap="gray")
+    plt.imsave("8.png", num, cmap="gray")
